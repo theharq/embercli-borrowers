54c09db114b255b180c2fa3faecdb343cc8bafdf
Hildebrando Rueda <harq.soft@gmail.com>
2015-02-02 18:11:54 -0500

    refresh model for sorting

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    sortBy: {
+      refreshModel: true
+    },
+    sortAscending: {
+      refreshModel: true
+    }
+  },
   model: function(){
     return this.store.find('friend');
   }
