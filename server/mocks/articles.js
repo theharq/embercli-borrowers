47a277451b80691fbf74801cb6c0523b8ea00cd0
Hildebrando Rueda <harq.soft@gmail.com>
2015-02-02 18:45:30 -0500

    Mock articles http request

---
+++ server/mocks/articles.js
@@ -0,0 +1,19 @@
+module.exports = function(app) {
+  var express = require('express');
+  var articlesRouter = express.Router();
+
+  articlesRouter.get('/', function(req, res) {
+    res.send({
+      "article":{
+        "id":74,
+        "created_at":"2014-11-03T21:30:47.869Z",
+        "description":"foo",
+        "state":"borrowed",
+        "notes":"bar",
+        "friend_id":153
+      }
+    });
+  });
+
+  app.use('/api/articles', articlesRouter);
+};
