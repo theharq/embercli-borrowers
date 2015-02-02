822bad63c83bdf13b0327e5419ef191f0e40d1e2
Hildebrando Rueda <harq.soft@gmail.com>
2015-02-02 14:31:13 -0500

    articles new route, actions and model hook
    we modify the article model to have a default state of ‘borrowed’

--- app/models/article.js
+++ app/models/article.js
@@ -5,5 +5,7 @@ export default DS.Model.extend({
   createdAt:    DS.attr('date'),
   description:  DS.attr('string'),
   notes:        DS.attr('string'),
-  state:        DS.attr('string')
+  state:        DS.attr('string',{
+    defaultValue: 'borrowed'
+  })
 });
