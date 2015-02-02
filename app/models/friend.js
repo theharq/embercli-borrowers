4fd0b447f486be40ede1b998da0699ea65a0ee20
Hildebrando Rueda <harq.soft@gmail.com>
2015-02-02 12:13:30 -0500

    set relationship between Friend and Article models

--- app/models/friend.js
+++ app/models/friend.js
@@ -2,11 +2,12 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.Model.extend({
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
-  email: DS.attr('string'),
-  twitter: DS.attr('string'),
-  totalArticles: DS.attr('number'),
+  articles:       DS.hasMany('article'),
+  firstName:      DS.attr('string'),
+  lastName:       DS.attr('string'),
+  email:          DS.attr('string'),
+  twitter:        DS.attr('string'),
+  totalArticles:  DS.attr('number'),
   fullName: Ember.computed('firstName', 'lastName', function() {
     return this.get('firstName') + ' ' + this.get('lastName');
   })
