ae5132092327bc231d0002ecfd05c41eb9d198e6
Hildebrando Rueda <harq.soft@gmail.com>
2015-02-02 16:49:05 -0500

    fix typo

--- app/helpers/formatted-date.js
+++ app/helpers/formatted-date.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 import {formatDate} from '../utils/date-helpers';
 
 export default Ember.Handlebars.makeBoundHelper(function(date, format){
-  return formatDate(date, format)
+  return formatDate(date, format);
 });
