335d0d632a17431c5c81be29fcd2e65fb91c05f8
Hildebrando Rueda <harq.soft@gmail.com>
2015-02-02 18:33:40 -0500

    updating tests

--- tests/unit/models/friend-test.js
+++ tests/unit/models/friend-test.js
@@ -1,15 +1,31 @@
-import {
-  moduleForModel,
-  test
-} from 'ember-qunit';
+import {moduleForModel,test} from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('friend', 'Friend', {
-  // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:article']
 });
 
 test('it exists', function() {
   var model = this.subject();
-  // var store = this.store();
-  ok(!!model);
+  ok(model);
 });
+
+test('fullName joins first and last name', function() {
+  var model = this.subject({firstName: 'Hilde', lastName: 'Rueda'})
+
+  equal(model.get('fullName'), 'Hilde Rueda')
+
+  Ember.run(function() {
+    model.set('firstName', 'Brando');
+  });
+
+  equal(model.get('fullName'), 'Brando Rueda', 'Updates fullName');
+});
+
+test('articles relationship', function() {
+  var klass = this.subject({}).constructor;
+  var relationship = Ember.get(klass, 'relationshipsByName').get('articles');
+  equal(relationship.key, 'articles');
+  equal(relationship.kind, 'hasMany');
+});
+
