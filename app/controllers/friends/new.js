a8afa93e31a0d74799be143c8a702881612cc854
Hildebrando Rueda <harq.soft@gmail.com>
2015-02-02 10:09:44 -0500

    Implement the save and cancel actions
    Validate the entries, set errorMessage if is not valid

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -1,12 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.email',
+    'model.firstName',
+    'model.lastName',
+    'model.twitter',
+    function(){
+      return !Ember.isEmpty(this.get('model.email')) && !Ember.isEmpty(this.get('model.firstName')) && !Ember.isEmpty(this.get('model.lastName')) && !Ember.isEmpty(this.get('model.twitter'));
+    }
+  ),
   actions: {
     save: function(){
-      console.log("save");
+      if (this.get('isValid')) {
+        var _this = this;
+        this.get('model').save().then(function(friend) {
+          _this.transitionToRoute('friends.show', friend);
+        });
+      } else {
+        this.set('errorMessage', 'You have to fill all the fields');
+      }
+      return false;
     },
     cancel: function(){
-      console.log("cancel");
+      this.transitionToRoute('friends');
+      return false;
     }
   }
 });
